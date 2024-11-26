0c19d2cd7db08930924983dacb61c6e3a1423f0b
IshaSheikh <89838836+IshaSheikh@users.noreply.github.com>
2024-11-26 10:30:06 +0530

    Add files via upload

---
+++ database.h
@@ -0,0 +1,7 @@
+#ifndef DATABASE_H_
+#define DATABASE_H_
+#include <sqLite3>
+
+
+
+#endif /*DATABSE_H_*/
---
+++ serv.cpp
@@ -0,0 +1,107 @@
+
+#include <Arduino.h>
+#include <WiFi.h>
+#include <WebSocketsServer.h>
+#include <SD.h>
+
+WebSocketsServer webSocket = WebSocketsServer(80);
+
+#define USE_SERIAL Serial
+
+void hexdump(const void *mem, uint32_t len, uint8_t cols = 16) {
+	const uint8_t* src = (const uint8_t*) mem;
+	USE_SERIAL.printf("\n[HEXDUMP] Address: 0x%08X len: 0x%X (%d)", (ptrdiff_t)src, len, len);
+	for(uint32_t i = 0; i < len; i++) {
+		if(i % cols == 0) {
+			USE_SERIAL.printf("\n[0x%08X] 0x%08X: ", (ptrdiff_t)src, i);
+		}
+		USE_SERIAL.printf("%02X ", *src);
+		src++;
+	}
+	USE_SERIAL.printf("\n");
+}
+
+void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t length) {
+
+    switch(type) {
+        case WStype_DISCONNECTED:
+            USE_SERIAL.printf("[%u] Disconnected!\n", num);
+            break;
+        case WStype_CONNECTED:
+            {
+                IPAddress ip = webSocket.remoteIP(num);
+                USE_SERIAL.printf("[%u] Connected from %d.%d.%d.%d url: %s\n", num, ip[0], ip[1], ip[2], ip[3], payload);
+
+		// send message to client
+		webSocket.sendTXT(num, "Connected");
+            }
+            break;
+        case WStype_TEXT:
+            USE_SERIAL.printf("[%u] get Text: %s\n", num, payload);
+            
+
+            // send message to client
+            // webSocket.sendTXT(num, "message here");
+
+            // send data to all connected clients
+            // webSocket.broadcastTXT("message here");
+            break;
+        case WStype_BIN:
+            USE_SERIAL.printf("[%u] get binary length: %u\n", num, length);
+            hexdump(payload, length);
+
+            // send message to client
+            // webSocket.sendBIN(num, payload, length);
+            break;
+	case WStype_ERROR:			
+	case WStype_FRAGMENT_TEXT_START:
+	case WStype_FRAGMENT_BIN_START:
+	case WStype_FRAGMENT:
+	case WStype_FRAGMENT_FIN:
+	    break;
+    }
+
+}
+
+void setup() {
+    // USE_SERIAL.begin(921600);
+    USE_SERIAL.begin(115200);
+
+    //Serial.setDebugOutput(true);
+    USE_SERIAL.setDebugOutput(true);
+
+    USE_SERIAL.println();
+    USE_SERIAL.println();
+    USE_SERIAL.println();
+
+    //----waiting
+    for(uint8_t t = 4; t > 0; t--) {
+        USE_SERIAL.printf("[SETUP] BOOT WAIT %d...\n", t);
+        USE_SERIAL.flush();
+        delay(1000);
+    }
+
+    WiFi.begin(ssid,password);
+    USE_SERIAL.print("Connecting to WiFi");
+    While(!WiFi.isConnected()){
+        USE_SERIAL.print(".");
+    }
+    USE_SERIAL.print("\nConnected!!\nIP Adress: ");
+    USE_SERIAL.prinln(WiFi.localIP());
+
+    USE_SERIAL.prinln(".......\nStarting websocket");
+    webSocket.begin();
+    webSocket.onEvent(webSocketEvent);
+
+    USE_SERIAL.println("connecting to SD card");
+    while(!SD.begin()){
+        USE_SERIAL.println("failed to connect to SDcard");
+        delay(1000);
+    }
+    USE_SERIAL.println("Connected to SDcard");
+
+}
+
+void loop() {
+    webSocket.loop();
+}
