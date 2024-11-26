0c6342de672b380d6a3591f363096f4bc29547a7
IshaSheikh <89838836+IshaSheikh@users.noreply.github.com>
2024-11-26 10:54:05 +0530

    Update and rename database.h to log.h

--- database.h
+++
@@ -1,7 +0,0 @@
-#ifndef DATABASE_H_
-#define DATABASE_H_
-#include <sqLite3>
-
-
-
-#endif /*DATABSE_H_*/
---
+++ log.h
@@ -0,0 +1,7 @@
+#ifndef LOG_H_
+#define LOG_H_
+#include <sqLite3>
+
+
+
+#endif /*LOG_H_*/
