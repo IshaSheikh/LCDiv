0223e4e042ef164675ee25678d05390506b06b5c
IshaSheikh <89838836+IshaSheikh@users.noreply.github.com>
2024-11-26 11:09:50 +0530

    Update log.h

--- log.h
+++ log.h
@@ -2,6 +2,11 @@
 #define LOG_H_
 #include <sqLite3>
 
-
+//usual log
+class logger {       // The class
+  public:             // Access specifier
+    logUserData(uint8_t id, char time, char date);
+    addUserData(char* name, char* designation);
+};
 
 #endif /*LOG_H_*/
